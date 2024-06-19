e8d2d71e9b037442cfe86b7293b7a02bbbe29b3d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-19 15:06:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ if selected == '📊':
         ,
         alt.Y('gebied:N',axis=alt.Axis(grid=False,domain=False,ticks=True,),sort=alt.EncodingSortField(field="gebied",  order='ascending'),title=None)
         ,
-        stroke=False,
+        stroke=None,
         # color=alt.Color('doel').title("Doel").legend(orient="right")
         # ,  
         fill=alt.Color('doel').title("Doel").legend(orient="right"),
