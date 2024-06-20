328a97846a204f5402fd8193eda97f1db02d8c59
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-06-20 11:33:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -144,19 +144,6 @@ if selected == '📊':
         use_container_width = True
     )
 
-    import numpy as np
-
-    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["col1", "col2", "col3"])
-    chart_data['col4'] = np.random.choice(['A','B','C'], 20)
-    
-    st.scatter_chart(
-        chart_data,
-        x='col1',
-        y='col2',
-        color='col4',
-        size='col3',
-    )
-    
 
 if selected == '📋':
 
